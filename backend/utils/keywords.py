67fd8f7c7036715e22dc24014aeb9b520a276f1f
Muskan <pooniamuskan4@gmail.com>
2025-11-30 22:46:15 +0530

    Delete backend directory

--- backend/utils/keywords.py
+++
@@ -1,7 +0,0 @@
-import yake
-
-extractor = yake.KeywordExtractor(lan="en", n=1, top=20)
-
-def extract_keywords(text, num=10):
-    kw = extractor.extract_keywords(text)
-    return [k[0] for k in kw[:num]]
--- backend/utils/translate.py
+++
@@ -1,9 +0,0 @@
-from googletrans import Translator
-
-translator = Translator()
-
-def translate_text(text, target_lang):
-    if target_lang is None:
-        return text
-    result = translator.translate(text, dest=target_lang)
-    return result.text
